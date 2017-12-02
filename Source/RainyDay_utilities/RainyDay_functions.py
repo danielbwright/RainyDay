3934846b2985698eed3fa44f2b40de9f23b926a6
Daniel Wright <danielb.wright@wisc.edu>
2017-12-02 11:21:32 -0600

    Changed license from GPLv3 to MIT

--- Source/RainyDay_utilities/RainyDay_functions.py
+++ Source/RainyDay_utilities/RainyDay_functions.py
@@ -5,7 +5,7 @@
 
 #    Welcome to RainyDay, a framework for coupling remote sensing precipitation
 #    fields with Stochastic Storm Transposition for assessment of rainfall-driven hazards.
-#    Version 3: Copyright (C) 2017  Daniel Benjamin Wright (danielb.wright@gwisc.edu)
+#    Copyright (C) 2017  Daniel Benjamin Wright (danielb.wright@gmail.com)
 #
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
